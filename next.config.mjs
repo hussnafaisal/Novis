3ca202301380e020fce6da2e01644cde992d8d00
hussna <hussnafaisal234@gmail.com>
2026-09-26 02:30:24 +0500

    Deploy Novis to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: "export",
-  trailingSlash: true,
-
   basePath: "/Novis",
   assetPrefix: "/Novis/",
-
   images: {
     unoptimized: true,
   },
