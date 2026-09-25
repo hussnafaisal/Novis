17cc99320d8d220738c47ceb5ba239c303d99ac3
hussna <hussnafaisal234@gmail.com>
2026-09-26 02:05:04 +0500

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
-/** @type {import("next").NextConfig} */
 const nextConfig = {
-  images: { remotePatterns: [{ protocol: "https", hostname: "images.unsplash.com" }] }
+  output: "export",
+  trailingSlash: true,
+
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: "/Novis",
+  assetPrefix: "/Novis/",
 };
+
 export default nextConfig;
